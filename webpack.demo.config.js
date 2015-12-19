aeb6d22bf7a8e185010a6c4499a5b1e8a8f01c1f
Tyler Kelley <tylerckelley@gmail.com>
2015-12-19 08:18:27 -0800

    updating demo build

---
+++ webpack.demo.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/example.js'
+  ],
+  output: {
+    path: __dirname + '/demo',
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules|dist/,
+        loader: 'babel'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader!sass-loader')
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('index.css')
+  ],
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, './src')]
+  }
+};
